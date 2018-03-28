347ee642c777ccb6960d4664277642985c1fe420
Thomas Rampelberg <thomas@saunter.org>
2018-03-28 12:41:43 -0500

    version bump

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-VERSION = "2.0.2"
+VERSION = "2.0.5"
 
 config = {
     'name': 'synse_graphql',
--- synse_graphql/__init__.py
+++ synse_graphql/__init__.py
@@ -2,13 +2,10 @@
 
     Author: Erick Daniszewski
     Date:   2 Oct 2017
-
-    \\//
-     \/apor IO
 """
 
 __title__ = 'synse-graphql'
-__version__ = '1.0.0'
+__version__ = '2.0.5'
 __description__ = 'Synse GraphQL'
 __author__ = 'Vapor IO'
 __author_email__ = 'vapor@vapor.io'
